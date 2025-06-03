83885ad7849b534a7f95d0cb64100bde6f74526b
Đỗ Minh Hùng <92613966+DoMinhHHung@users.noreply.github.com>
2025-06-03 12:02:51 +0700

    Implement Frontend Interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AuthPage from "./pages/AuthPage";
-import BoardManagement from "./pages/BoardManagement";
-import GithubCallback from "./components/Auth/GithubCallback";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import BoardManagement from './pages/BoardManagement';
+import GithubCallback from './components/Auth/GithubCallback';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route path="/boards" element={<BoardManagement />} />
-        <Route path="/auth/github/callback" element={<GithubCallback />} />
-      </Routes>
-    </BrowserRouter>
+    <DndProvider backend={HTML5Backend}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthPage />} />
+          <Route path="/boards" element={<BoardManagement />} />
+          <Route path="/auth/github/callback" element={<GithubCallback />} />
+        </Routes>
+      </BrowserRouter>
+    </DndProvider>
   );
 }
 
